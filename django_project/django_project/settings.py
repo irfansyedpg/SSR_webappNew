4e105f8e38062370a6b6aaa8e1eda9b81d4d1772
syed.irfanullah <irfansyed.pg@gmail.com>
2019-12-13 18:01:23 +0500

    commit

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ur-rPK' # change language fom us-en to ur_rPK
 
 TIME_ZONE = 'UTC'
 
